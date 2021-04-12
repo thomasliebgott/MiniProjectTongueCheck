22998e0ce798f1046e8a1cd47bf359f487fae939
thomasliebgott <thomas.liebgott@live.fr>
2021-04-12 18:40:29 +0200

    12 avril 18h40

--- app/src/main/java/com/example/miniprojectliebgott/MainActivity.java
+++ app/src/main/java/com/example/miniprojectliebgott/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
+    // test commit 18h40
     @Override
     protected void onResume() {
         super.onResume();
